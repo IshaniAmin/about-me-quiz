9321c6bab8c81c23e5d9fd1b656b93322e4d9f5a
Ishani Amin <ishaniamin@gmail.com>
2018-08-14 17:45:21 +0000

    added some comments to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < Sinatra::Base
   
   post '/results' do
     #puts params
+    # 'params' gives you a hash of all the values
     points_array = params.values # makes an array that has all the point values from the questions answered
     total = 0
     points_array.each do |point| # loop through points array to calculate total
@@ -20,6 +21,7 @@ class ApplicationController < Sinatra::Base
     end
     #puts total
     
+    # require rb file at top to be used here
     result = calculate_result(total) # use the method calculate_result (that we defined in sample_model.rb) to get result
     
     #puts result
